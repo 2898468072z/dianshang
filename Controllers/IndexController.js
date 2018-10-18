0416bde8ad0d1176b2780a8a1c27e822444bea08
2898468072z <18398544832@163.com>
2018-10-18 09:35:50 +0800

    更新2

--- Controllers/IndexController.js
+++ Controllers/IndexController.js
@@ -1,34 +1,6 @@
 exports.index=function(req,res){
-    if(req.session.sign){
-        res.render('index',{state:2});
-        return;
-    }
-
-    var username =req.cookies.username;
-    var password=req.cookies.password;
-
-    if(username==null||password==null){
-        res.render('index',{state:-1});
-    }else{
-        //(1)引入userService
-        var UserService = require('../Service/UserService');
-        //(2)创建对象
-        var userService = new UserService();
-        //(3)对象初始化
-        userService.init();
-        //(4)验证用户都合法
-        userService.checkUser(name,password,function(result){
-            if(result.state==2)
-            {
-                req.session.sign=true;
-                res.render('index',{state:2});
-            }else{
-                res.render('index',{state:-1});
-            }
-        },1);
-    }
+    res.render('index',{});
 }
-
 exports.login=function(req,res){
     //1,解析客户端提交的数据
     var username  = req.body.username;
@@ -42,14 +14,12 @@ exports.login=function(req,res){
     userService.init();
     //(4)验证用户是否合法
     userService.checkUser(username,password,function(result){
-        if(result.state==2){
-            req.session.sign=true;
-            res.cookie('username',result.username, {maxAge:60*60*1000});
-            res.cookie('password',result.password, {maxAge:60*60*1000});
-        }
-        result.username=null;
-        result.password=null;
         res.end(JSON.stringify(result));
-    },0);
+    });
+
+
+
+
+
 
 }
--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser');
-app.use(cookieParser());
-var session = require('express-session');
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 //2,引入的ejs插件
 var ejs = require('ejs');
@@ -18,19 +15,21 @@ app.engine('html', require('ejs').__express);
 //6,静态文件
 app.use(express.static('public'));
 
-app.use(session({
-    secret: '12345',
-    name: '欢迎登陆',   //这里的name值得是cookie的name，默认cookie的name是：connect.sid
-    cookie: {maxAge: 24*60*60*1000 },     //设置maxAge是80000ms，即80s后session和相应的cookie失效过期
-}));
-
-
-
 var indexController=require('./Controllers/IndexController');
 app.get('/index',indexController.index);
 app.post('/login',urlencodedParser,indexController.login);
 var indexController = require('./Controllers/IndexController1');
 app.post('/signup',urlencodedParser,indexController.signup);
+
+var indexController=require('./Controllers/IndexController2');
+app.get('/product-m',indexController.index);
+var indexController=require('./Controllers/IndexController2');
+app.get('/product',indexController.product);
+
+var indexController=require('./Controllers/IndexController2');
+app.get('/product-k',indexController.product_k);
+var indexController=require('./Controllers/IndexController3');
+app.get('/checkout',indexController.checkout);
 
 
 
@@ -38,24 +37,11 @@ app.post('/signup',urlencodedParser,indexController.signup);
 app.get('/login',function (req,res) {
     res.render("login");
 });
-app.get('/checkout',function (req,res) {
-    res.render("checkout");
-});
 app.get('/contact',function (req,res) {
     res.render("contact");
 });
-
 app.get('/pressroom',function (req,res) {
     res.render("pressroom");
-});
-app.get('/product',function (req,res) {
-    res.render("product");
-});
-app.get('/product-k',function (req,res) {
-    res.render("product-k");
-});
-app.get('/product-m',function (req,res) {
-    res.render("product-m");
 });
 app.get('/shortcodes',function (req,res) {
     res.render("shortcodes");
